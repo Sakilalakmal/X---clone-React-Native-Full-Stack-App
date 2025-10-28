dce1c9f1a9caee2a36cd7321ce2b18fd51fcc44f
sakilalakmal <sakilalakmal77@gmail.com>
2025-10-28 19:14:43 +0530

    comment routes and controlles added

---
+++ backend/src/routes/comment.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  getCommentForPost,
+  createCommentForPost,
+  deleteComment,
+} = require("../controllers/comment.controller");
+const { isAuth } = require("../middlewares/iaAuth.middleware");
+
+const commentRouter = express.Router();
+
+//public route
+
+commentRouter.get("/post/:postId", getCommentForPost);
+
+//private route
+
+commentRouter.get("/create/post/:postId", isAuth, createCommentForPost);
+
+commentRouter.delete("/:commentId", isAuth, deleteComment);
+
+module.exports = commentRouter;
