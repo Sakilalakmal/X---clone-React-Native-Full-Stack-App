7ac86bd605fc2c0c447c6e8e2d8fb4f82d1e3e99
sakilalakmal <sakilalakmal77@gmail.com>
2025-10-29 13:25:06 +0530

    added auth with clerk functions

---
+++ mobile/hooks/useSocialAuth.ts
@@ -0,0 +1,30 @@
+import { useSSO } from "@clerk/clerk-expo";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+export const useSocialAuth = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { startSSOFlow } = useSSO();
+
+  const handleSocialAuth = async (strategy: "oauth_google" | "oauth_apple") => {
+    setIsLoading(true);
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({ strategy });
+      if (createdSessionId && setActive) {
+        await setActive({ session: createdSessionId });
+      }
+    } catch (error) {
+      console.log("error in social Auth", error);
+      const provider = strategy === "oauth_google" ? "Google" : "Apple";
+      Alert.alert(
+        "Authentication Error",
+        `There was an issue signing in with ${provider}. Please try again.`
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { isLoading, handleSocialAuth };
+};
